eb54c10f42bbf36e975cd56ceb8b2c1f9cb1971c
Nikki A <nikki.apostolakis@gmail.com>
2019-05-05 22:30:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
+  let repoList = '<ul>';
   for (var i = 0; i < this.responseText.length; i++) {
     repoList += '<li>' + this.responseText[i]['name'] + '</li>';
   }
